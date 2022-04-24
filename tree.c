e8e7101ef69a398abe196c730f98daced79eb144
Shintarou Okumura <flekystyley@gmail.com>
2022-04-24 23:18:27 +0900

    create tree.c

---
+++ tree.c
@@ -0,0 +1,9 @@
+#include "data.h"
+#include "ast.h"
+#include "decl.h"
+
+// build and return generic ast node
+struct ASTnode *new_ast_node(int op, struct ASTnode *left,
+						   struct ASTnode *right, int intvale) {
+	struct ASTnode *node;
+}
