8bf8846173ac7b0eccf6363a61bee15cb35968a3
Shintarou Okumura <flekystyley@gmail.com>
2022-04-24 23:18:20 +0900

    create ast.h

---
+++ ast.h
@@ -0,0 +1,20 @@
+// AST node types
+enum {
+	AST_ADD, // '+'
+	AST_SUBSTRACT, // '-'
+	AST_MULTIPLY, // '*'
+	AST_DIVIDE, // '/'
+	AST_INTLIT // interger literal
+};
+
+// Abstract Syntax Tree structure
+// op値A_INTLITを持つASTノードは整数値を表す
+// これはサブツリーの子を持たず、intvalueフィールドに値を持つだけ
+// 左と右のツリー. 後で、子木の値を加算したり減算したりするために右と左で分ける
+//
+struct ASTnode {
+	int op; // ツリーに対して行う操作
+	struct ASTnode *left;
+	struct ASTnode *right;
+	int intvalue; // AST_INTLITの場合の整数値
+};
