4d8a2b49a50d698fb95c8856bedf03fd11c744c3
Shintarou Okumura <flekystyley@gmail.com>
2022-05-04 15:23:42 +0900

    update data.h

--- data.h
+++ data.h
@@ -2,12 +2,6 @@
  #define extern_ extern
 #endif
 
-// Global variables
-// Copyright (c) 2019 Warren Toomey, GPL3
-
-// Global variables
-// Copyright (c) 2019 Warren Toomey, GPL3
-
 // 行数
 extern_ int Line;
 // 文字列を戻す
