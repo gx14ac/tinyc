9d807d1d1c1d216e9dbc0bc5b5cd43d57f8e7b61
shinta <flekystyley@gmail.com>
2022-05-12 13:16:52 +0900

    fix

--- scan.c
+++ scan.c
@@ -47,6 +47,40 @@ static int skip(void) {
 	return c;
 }
 
+// 英数字をバッファに読み込んで、英数字以外の文字にぶつかるまで読み込む
+static int scanident(int c, char *buf, int lim) {
+	int i = 0;
+
+	// 数字、アルファベット、アンダースコアは許可する
+	while (isalpha(c) || isdigit(c) || '_' == c) {
+		// 識別子の長さ制限
+		if (lim - 1 == i) {
+			printf("identfier too long on line %d\n", Line);
+			exit(1);
+		} else if (i < lim - 1) {
+			buf[i++] = c;
+		}
+		c = next();
+	}
+
+	// 有効でない文字に当たったので元に戻す
+  	// buf[]をNUL終端し、長さを返す. これで有効な文字列と有効でない文字列を分けることができる
+	put_back(c);
+	buf[i] = '\0';
+	return i;
+}
+
+static int keyword(char *s) {
+	switch (*s) {
+		case 'p':
+			if (!strcmp(s, "print")) {
+				return TOKEN_PRINT;
+				break;
+			}
+	}
+	return 0;
+}
+
 // 10進数文字の時にスキャンした文字列を整数で返す
 static int scan_int(int c) {
 	int k, val = 0;
@@ -116,38 +150,4 @@ int scan(struct token *t) {
 	}
 
 	return 1;
-}
-
-// 英数字をバッファに読み込んで、英数字以外の文字にぶつかるまで読み込む
-static int scanident(int c, char *buf, int lim) {
-	int i = 0;
-
-	// 数字、アルファベット、アンダースコアは許可する
-	while (isalpha(c) || isdigit(c) || '_' == c) {
-		// 識別子の長さ制限
-		if (lim - 1 == i) {
-			printf("identfier too long on line %d\n", Line);
-			exit(1);
-		} else if (i < lim - 1) {
-			buf[i++] = c;
-		}
-		c = next();
-	}
-
-	// 有効でない文字に当たったので元に戻す
-  	// buf[]をNUL終端し、長さを返す. これで有効な文字列と有効でない文字列を分けることができる
-	put_back(c);
-	buf[i] = '\0';
-	return i;
-}
-
-static int keyword(char *s) {
-	switch (*s) {
-		case 'p':
-			if (!strcmp(s, "print")) {
-				return TOKEN_PRINT;
-				break;
-			}
-	}
-	return 0;
 }
