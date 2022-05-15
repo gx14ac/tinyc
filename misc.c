f41e6cbf57bfe9e931695f33eb147ea06becd89f
shinta <flekystyley@gmail.com>
2022-05-15 15:42:14 +0900

    fix

--- misc.c
+++ misc.c
@@ -7,8 +7,7 @@ void match(int t, char *what) {
     if (Token.token == t) {
         scan(&Token);
     } else {
-        printf("%s expected on line %d\n", what, Line);
-        exit(1);
+      fatals("expected", what);
     }
 }
 
